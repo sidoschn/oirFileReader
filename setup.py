ebc817fbbb8d4a664da8eab4cd695d87a3e8622e
sidoschn <dominik.schneidereit@fau.de>
2024-10-11 15:36:39 +0200

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,9 @@ setup(
   download_url = 'https://github.com/sidoschn/oirFileReader/archive/refs/tags/v0.1.tar.gz',    # I explain this later on
   keywords = ['Olympus', 'OIR', '.oir', 'reader'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'xmltodict',
+          'flatdict',
+          'numpy',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
