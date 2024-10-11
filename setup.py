29d7a6cd3bb166978ad7385220d80319c7d189c5
sidoschn <dominik.schneidereit@fau.de>
2024-10-11 16:08:30 +0200

    debugged setup.py for use with twine

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'oirFileReader',         # How you named your package folder (MyLib)
   packages = ['oirFileReader'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
-  license='GPLv3',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  license='GPL-3.0-only',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Read Olympus .oir image files in python',   # Give a short description about your library
   author = 'Dominik Schneidereit',                   # Type in your name
   author_email = 'dominik.schneidereit@gmail.com',      # Type in your E-Mail
@@ -19,7 +19,6 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'OSI Approved :: GNU General Public License v3 (GPLv3)',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
   ],
 )
